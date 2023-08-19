a2ea42af29abb04cd0517e6bae2c62345016cde4
Mechanisms Robotics <micahmaphet@gmail.com.com>
2023-08-19 13:51:06 -0400

    added controller input to the intake

--- src/main/java/frc/robot/commands/IntakeCommand.java
+++ src/main/java/frc/robot/commands/IntakeCommand.java
@@ -1,10 +1,15 @@
 package frc.robot.commands;
 
+import edu.wpi.first.wpilibj.PS4Controller;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.Intake;
 
 public class IntakeCommand extends CommandBase {
     private final Intake intake;
+
+    private final double pivotSpeed = 0.025;
+
+    PS4Controller PS4Cont = new PS4Controller(0);
 
     public IntakeCommand(Intake intake) {
         this.intake = intake;
@@ -13,7 +18,8 @@ public class IntakeCommand extends CommandBase {
     
     @Override
     public void execute() {
-        intake.setDirection(20);
+        // Move the intake if the square button is pressed
+        intake.setPivotSpeed(PS4Cont.getSquareButton() ? pivotSpeed : 0);
     }
     
 }
